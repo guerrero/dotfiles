1baf55f9e953a9e5ebd29373c6785ee600ab4cfb
Aloge <alexguerrero1092@gmail.com>
2014-01-07 14:35:41 +0100

    Minor changes

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -91,3 +91,10 @@ export SAVEHIST=$HISTSIZE
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+
+# Larger bash history (allow 32³ entries; default is 500)
+export HISTSIZE=32768
+export HISTFILESIZE=$HISTSIZE
+export HISTCONTROL=ignoredups
+# Make some commands not show up in history
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
