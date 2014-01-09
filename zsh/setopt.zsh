6dc7cf31cf99d24f961390a74858ab84bfed1bc7
Aloge <alexguerrero1092@gmail.com>
2014-01-09 11:57:02 +0100

    Remove unnecesary options and add comments

--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -1,83 +1,85 @@
-# Now we can pipe to multiple outputs!
-# setopt MULTIOS
+# why would you type 'cd dir' if you could just type 'dir'?
+setopt auto_cd
 
-# Spell check commands!  (Sometimes annoying)
-setopt CORRECT
+# If the argument to a cd command (or an implied cd with the AUTO_CD option set) is not a directory, and does not begin with a slash, try to expand the expression as if it were preceded by a '~' (see the section 'Filename Expansion'). 
+setopt cdable_vars
 
-# This makes cd=pushd
-setopt AUTO_PUSHD
+# When changing to a directory containing a path segment '..' which would otherwise be treated as canceling the previous segment in the path (in other words, 'foo/..' would be removed from the path, or if '..' is the first part of the path, the last part of $PWD would be deleted), instead resolve the path to the physical directory
+setopt chase_dots
+
+# Resolve symbolic links to their true values when changing directory
+setopt chase_links
+
+# Don't push multiple copies of the same directory onto the directory stack
+setopt pushd_ignore_dups
+
+# Have pushd with no arguments act like 'pushd $HOME'
+setopt pushd_to_home
+
+# Exchanges the meanings of '+' and '-' when used with a number to specify a directory in the stack
+setopt pushd_minus
+
+# Automatically list choices on an ambiguous completion. (default)
+setopt auto_list
 
 # This will use named dirs when possible
-setopt AUTO_NAME_DIRS
+setopt auto_name_dirs
 
-# If we have a glob this will expand it
-setopt GLOB_COMPLETE
-setopt PUSHD_MINUS
+# I like annoying pushd messages...
+setopt no_pushd_silent
 
-# No more annoying pushd messages...
-# setopt PUSHD_SILENT
+# 10 second wait if you do 'rm *' or 'rm path/*'.
+setopt rm_star_wait
 
-# blank pushd goes to home
-setopt PUSHD_TO_HOME
+# use zsh line editor (this is default, but it can't hurt!)
+setopt zle
 
-# this will ignore multiple directories for the stack.  Useful?  I dunno.
-setopt PUSHD_IGNORE_DUPS
+# Allow comments even in interactive shells
+setopt interactive_comments
 
-# 10 second wait if you do something that will delete everything.  I wish I'd had this before...
-setopt RM_STAR_WAIT
+# Beeps are annoying
+setopt no_beep
 
-# use magic (this is default, but it can't hurt!)
-setopt ZLE
+# Do not exit on end-of-file
+setopt ignore_eof
 
-setopt NO_HUP
-
-setopt VI
-
-setopt IGNORE_EOF
-
-# If I could disable Ctrl-s completely I would!
-setopt NO_FLOW_CONTROL
-
-# beeps are annoying
-setopt NO_BEEP
-
-# Keep echo "station" > station from clobbering station
-setopt NO_CLOBBER
-
-# Case insensitive globbing
-setopt NO_CASE_GLOB
-
-# Be Reasonable!
-setopt NUMERIC_GLOB_SORT
-
-# I don't know why I never set this before.
-setopt EXTENDED_GLOB
-
-# hows about arrays be awesome?  (that is, frew${cool}frew has frew surrounding all the variables, not just first and last
-setopt RC_EXPAND_PARAM
-
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-
-
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
+# Don't expand aliases _before_ completion has finished
 setopt complete_aliases
 
-zle -N newtab
+# Disable output flow control in shell's editor via start/stop characters (^S/^Q) 
+setopt no_flow_control
 
+# Treat the '#', '~' and '^' as part of patterns for filename generation
+setopt extended_glob
 
+# If numeric filenames are matched by a filename generation pattern, sort the filenames numerically rather than lexicographically. 
+setopt numeric_glob_sort
 
-# Initialize completion
-autoload -U compinit
-compinit
+# TEST:
+
+# Now we can pipe to multiple outputs!
+# setopt multios
+
+# Don't nice background tasks
+# setopt no_bg_nice
+
+# ???
+# setopt complete_in_word
+
+# If we have a glob this will expand it
+# setopt glob_complete
+
+# Why???
+# setopt no_hup
+
+# Fuck
+# setopt exec
+
+# Fuck
+# setopt function_argzero
+
+# hows about arrays be awesome?  (that is, frew${cool}frew has frew surrounding all the variables, not just first and last
+# setopt rc_expand_param
+
+# Fuck
+# setopt correct
