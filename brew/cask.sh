a2a1371f932e6ab0e71445390c5e10b7a408c5a4
Alex Guerrero <alexguerrero1092@gmail.com>
2014-11-04 13:43:50 +0100

    Deprecated , use  instead

--- brew/cask.sh
+++ brew/cask.sh
@@ -1,49 +1,54 @@
+#!/usr/bin/env bash
+
+# Install apps using Homebrew-Cask (http://caskroom.io/)
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 #Update Homebrew
-update
+brew update && brew upgrade
 
 # Install Homebrew Cask
-install caskroom/cask/brew-cask
+brew install caskroom/cask/brew-cask
 
 # Add support for alternate versions of Casks
-tap caskroom/versions
+brew tap caskroom/versions
 
 # Install applications
-cask install alfred
-cask install arduino
-cask install clamxav
-cask install cleanmymac
-cask install evasi0n
-cask install flux
-cask install firefox
-cask install flash
-cask install google-chrome
-cask install hydra
-cask install iterm2
-cask install licecap
-cask install mactracker
-cask install processing
-cask install skype
-cask install slate
-cask install spotify
-cask install torbrowser
-cask install transmission
-cask install vienna-rss
-cask install virtualbox
-cask install vlc
+brew cask install alfred
+brew cask install arduino
+brew cask install clamxav
+brew cask install cleanmymac
+brew cask install cyberduck
+brew cask install flux
+brew cask install firefox
+brew cask install flash
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install licecap
+brew cask install mactracker
+brew cask install processing
+brew cask install skype
+brew cask install slate
+brew cask install spotify
+brew cask install torbrowser
+brew cask install transmission
+brew cask install virtualbox
+brew cask install vlc
 
 # Add Caskroom to alfred search paths
-cask alfred link
+brew cask alfred link
 
 # Install Quick Look plugins
-cask install qlcolorcode # Enable syntax highlighting in source files
-cask install qlstephen # Preview plain text files without extension
-cask install qlmarkdown # Preview Markdown files
-cask install quicklook-json # Preview json files
-qlmanage -r # reset Quick Look extension manager
-
-# Upgrade any already-installed formulae
-upgrade
+brew cask install qlcolorcode     # Syntax highlighting in source files
+brew cask install qlstephen       # Plain text files preview
+brew cask install qlmarkdown      # Markdown files preview
+brew cask install quicklook-json  # JSON files preview
+qlmanage -r                       # reset QL extension manager
 
 # Remove outdated versions from the cellar
-cleanup
-cask cleanup
+brew cleanup
+brew cask cleanup
