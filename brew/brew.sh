2df06f7e5785e3aad468fd9132fee83951adedde
AG <Alex@MacBook-Pro-de-AG.local>
2014-11-18 19:26:45 +0100

    Add some formulas from  and remove others

--- brew/brew.sh
+++ brew/brew.sh
@@ -5,7 +5,7 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
+# Keep-alive: update existing `sudo` time stamp until `brew.sh` has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we're using the latest Homebrew
@@ -18,30 +18,29 @@ brew upgrade
 # Add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`
 # to use them without prefixes
 brew install coreutils --default-names
-brew install findutils --default-names
+brew install findutils --with-default-names
 
 # Install wget with IRI support
-brew install wget --enable-iri
+brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install vim --with-lua --override-system-vi
 brew install homebrew/dupes/grep
 
 # Install everything else
-brew install tmux
+brew install duti
 brew install fasd
 brew install git
 brew install git-extras
+brew install go
 brew install node
+brew install python3
 brew install rename
 brew install tree
-brew install go
-brew install ino
-brew install python3
 # brew install hub
-
-# Remove outdated versions from the cellar
-brew cleanup
 
 # Check if installation is successfully
 brew doctor
+
+# Remove outdated versions from the cellar
+brew cleanup
