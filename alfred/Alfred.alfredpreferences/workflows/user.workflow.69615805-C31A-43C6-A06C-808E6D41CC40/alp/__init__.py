dff65bfe2caa35445097283308b0b33e6da0428c
Alex Guerrero <alexguerrero1092@gmail.com>
2014-12-17 07:32:32 +0100

    Remove Alfred preferences

--- alfred/Alfred.alfredpreferences/workflows/user.workflow.69615805-C31A-43C6-A06C-808E6D41CC40/alp/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    from .core import *
-    from .item import *
-    from .keychain import *
-    from .settings import *
-    from .mail import *
-    from .notification import *
-except ImportError:
-    pass
