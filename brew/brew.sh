2f5a4a6fa566e0abcdd2a6836f29ff1345c8ff55
Alex Guerrero <alexguerrero1092@gmail.com>
2015-07-06 13:44:04 +0200

    Remove unnecesary request for admin privileges

--- brew/brew.sh
+++ brew/brew.sh
@@ -2,12 +2,6 @@
 
 # Install binary packages using Homebrew (http://brew.sh)
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `brew.sh` has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Make sure we're using the latest Homebrew
 brew update
 
