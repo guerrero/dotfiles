11d1833ae6179afe2ba0a3b2699fceb58f87f64b
Alex Guerrero <alexguerrero1092@gmail.com>
2015-08-11 14:36:46 +0200

    Remove pyhton3 install command and brew doctor

--- brew/brew.sh
+++ brew/brew.sh
@@ -21,15 +21,11 @@ brew install gist
 brew install go
 brew install node
 brew install iojs
-brew install python3
 brew install tree
 brew install hub
 
 # Install OpenCV
 brew install homebrew/science/opencv
-
-# Check if installation is successfully
-brew doctor
 
 # Remove outdated versions from the cellar
 brew cleanup
