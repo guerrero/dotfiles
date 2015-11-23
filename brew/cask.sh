b59c951ce6990426808d4f09ec887d189ac35b0e
Alex Guerrero <alexguerrero1092@gmail.com>
2015-11-23 20:26:44 +0100

    Update installed casks
    - Ensure that Hombrew is up to date before install Homebrew-cask
    - Remove flash
    - Add Silverlight to support Netflix
    - Add some apps
    - Add some fonts

--- brew/cask.sh
+++ brew/cask.sh
@@ -8,6 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `cask.sh` has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Update Homebrew to ensure we are using the latest Homebrew-cask version
+brew update
+
 # Intall Homebrew Cask
 brew install caskroom/cask/brew-cask
 
@@ -20,6 +23,9 @@ brew tap caskroom/versions
 # Install Java 6 required by some apps
 brew cask install java6
 
+# Install Silverlight required by Netflix
+brew cask install silverlight
+
 # Install applications
 brew cask install alfred
 brew cask install atom
@@ -27,19 +33,21 @@ brew cask install clamxav
 brew cask install cleanmymac
 brew cask install cyberduck
 brew cask install dash
-brew cask install flux
 brew cask install firefox
-brew cask install flash
+brew cask install firefoxdeveloperedition
+brew cask install flux
 brew cask install google-chrome
-brew cask install iterm2
+brew cask install google-hangouts
 brew cask install imageoptim
+brew cask install iterm2
 brew cask install licecap
 brew cask install mactracker
 brew cask install skype
 brew cask install slack
 brew cask install slate
-brew cask install sublime-text3
 brew cask install spotify
+brew cask install sublime-text3
+brew cask install telegram
 brew cask install torbrowser
 brew cask install transmission
 brew cask install virtualbox
@@ -49,14 +57,22 @@ brew cask install vlc
 brew tap caskroom/fonts
 
 # Install fonts
+brew cask install font-cabin
 brew cask install font-fira-sans
+brew cask install font-lato
+brew cask install font-merriweather
+brew cask install font-merriweather-sans
+brew cask install font-montserrat
 brew cask install font-open-sans
+brew cask install font-oxygen
 brew cask install font-pt-sans
 brew cask install font-pt-serif
+brew cask install font-quattrocento-sans
 brew cask install font-roboto
 brew cask install font-source-sans-pro
 brew cask install font-source-serif-pro
-
+brew cask install font-varela
+brew cask install font-varela-round
 
 # Remove outdated versions from the cellar
 brew cleanup
