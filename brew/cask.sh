f7b1031bf779cfe1c1a6ecbd62d7958c778334ed
Alex Guerrero <alexguerrero1092@gmail.com>
2016-08-29 18:44:18 +0200

    Fix homebrew detection condition and delete default app-dir tweak

--- brew/cask.sh
+++ brew/cask.sh
@@ -12,71 +12,77 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew update
 
 # Intall Homebrew Cask
-brew install caskroom/cask/brew-cask
-
-# Changes the path where symlinks to the cask applications will be generated
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+brew tap caskroom/cask
 
 # Tap homebrew cask versions to enable installation of alternate Cask versions
 brew tap caskroom/versions
 
+install() {
+  for cask in $@; do
+    brew cask install $cask
+  done
+}
+
 # Install Java 6 required by some apps
-brew cask install java6
+install java6
 
 # Install Silverlight required by Netflix
-brew cask install silverlight
+install silverlight
 
 # Install applications
-brew cask install alfred
-brew cask install atom
-brew cask install clamxav
-brew cask install cleanmymac
-brew cask install cyberduck
-brew cask install dash
-brew cask install firefox
-brew cask install firefoxdeveloperedition
-brew cask install flux
-brew cask install google-chrome
-brew cask install google-hangouts
-brew cask install imageoptim
-brew cask install iterm2
-brew cask install licecap
-brew cask install mactracker
-brew cask install sketch
-brew cask install skype
-brew cask install slack
-brew cask install slate
-brew cask install spotify
-brew cask install sublime-text3
-brew cask install telegram
-brew cask install torbrowser
-brew cask install transmission
-brew cask install virtualbox
-brew cask install vlc
-brew cask install zeplin
-brew cask install screenhero
+install alfred
+install atom
+install clamxav
+install cleanmymac
+install cyberduck
+install dash
+install firefox
+install flux
+install google-chrome
+install chromium
+install google-hangouts
+install imageoptim
+install iterm2
+install licecap
+install mactracker
+install sketch
+install skype
+install slack
+install hammerspoon
+install spotify
+install sublime-text3
+install telegram
+install torbrowser
+install transmission
+install virtualbox
+install vlc
+install zeplin
+install screenhero
 
 # Tap homebrew cask fonts to enable installation of binary font files
 brew tap caskroom/fonts
 
 # Install fonts
-brew cask install font-cabin
-brew cask install font-fira-sans
-brew cask install font-lato
-brew cask install font-merriweather
-brew cask install font-merriweather-sans
-brew cask install font-montserrat
-brew cask install font-open-sans
-brew cask install font-oxygen
-brew cask install font-pt-sans
-brew cask install font-pt-serif
-brew cask install font-quattrocento-sans
-brew cask install font-roboto
-brew cask install font-source-sans-pro
-brew cask install font-source-serif-pro
-brew cask install font-varela
-brew cask install font-varela-round
+install font-cabin
+install font-fira-sans
+install font-lato
+install font-merriweather
+install font-merriweather-sans
+install font-montserrat
+install font-open-sans
+install font-oxygen
+install font-pt-sans
+install font-pt-serif
+install font-quattrocento-sans
+install font-roboto
+install font-source-sans-pro
+install font-source-serif-pro
+install font-varela
+install font-varela-round
+install font-inconsolata
 
 # Remove outdated versions from the cellar
 brew cleanup
 brew cask cleanup
+
+unset -f install
--- fish/modules/homebrew/init.fish
+++ fish/modules/homebrew/init.fish
@@ -3,19 +3,10 @@
 #
 
 # Return if requirements are not found.
-if test (uname) = 'Darwin'
-  and type -q brew
+if not test (uname) = 'Darwin'
+  or not type -q brew
   exit
 end
-
-
-#
-# Tweaks
-#
-
-# Place Homebrew-Cask app links into /Applications by default
-set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
-
 
 #
 # Aliases
