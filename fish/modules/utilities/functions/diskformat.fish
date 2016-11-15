5db6e79878f897e9fd95c321608348e337f6334f
Alex Guerrero <alexguerrero1092@gmail.com>
2016-11-15 12:32:29 +0100

    Add function to format disk using MS-DOS format

---
+++ fish/modules/utilities/functions/diskformat.fish
@@ -0,0 +1,6 @@
+function diskformat -d "Format a disk with MS-DOS (FAT) format"
+  set disk_number $argv[1]
+  set disk_name $argv[2]
+
+  diskutil eraseDisk FAT32 $disk_name /dev/disk$disk_number
+end
