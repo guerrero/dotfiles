5de189efbd50ea508edeebff4758a4b9c427ccf4
Alex Guerrero <hello@alexguerrero.es>
2017-06-04 16:31:11 +0200

    Prevent search in fish folder to find fish modules to avoid bugs

--- fish/config.fish
+++ fish/config.fish
@@ -13,17 +13,21 @@ for path in $dotfiles_dir/*
     end
 
     for module_function in $path/functions/*.fish
-      set -l function_name (string split -r -m1 / $module_function)[2]
-      set -l symlink_path $fish_real_dir/functions/$function_name
-      set -l symlink_reference (readlink $symlink_path)
+      set -l function_dirname (dirname $module_function)
 
-      if test -e $symlink_path
-        if test $symlink_reference != $module_function
-          set_color yellow; echo -n "Warning: "; set_color normal
-          echo "Duplicated fish function for $function_name."
+      if test $function_dirname != $fish_real_dir"functions"
+        set -l function_name (string split -r -m1 / $module_function)[2]
+        set -l symlink_path $fish_real_dir/functions/$function_name
+        set -l symlink_reference (readlink $symlink_path)
+
+        if test -e $symlink_path
+          if test $symlink_reference != $module_function
+            set_color yellow; echo -n "Warning: "; set_color normal
+            echo "Duplicated fish function for $function_name."
+          end
+        else
+          ln -s $module_function $symlink_path
         end
-      else
-        ln -s $module_function $symlink_path
       end
     end
   end
