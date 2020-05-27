07ebea38035426e05aa2a18d39d2de7e94a238ea
Alex Guerrero <hello@alexguerrero.es>
2020-05-27 05:46:34 +0200

    Improve awake function to enable to keep it forever

--- modules/utilities/functions/awake.fish
+++ modules/utilities/functions/awake.fish
@@ -1,7 +1,11 @@
-function awake -d "Prevent the system from sleeping during specified time in hours"
-  set hours $argv[1]
-  set seconds (math $hours \* 3600)
+function awake --description "Prevent the system from sleeping during specified time in hours" --argument-names hours
+  if test -n "$hours"
+    echo "Keeping computer awake for $hours hours"
 
-  echo "Keeping computer awake for $hours hours"
-  caffeinate -ut $seconds
+    set seconds (math $hours \* 3600)
+    caffeinate -ut $seconds
+  else
+    echo "Keeping computer awake forever. You can stop this process using ctrl + C."
+    caffeinate -d
+  end
 end
