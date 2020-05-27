bd846fb0c4346ea819de48a84813572619756462
Alex Guerrero <hello@alexguerrero.es>
2020-05-27 05:46:01 +0200

    Update fish variables

--- modules/utilities/functions/awake.fish
+++ modules/utilities/functions/awake.fish
@@ -2,5 +2,6 @@ function awake -d "Prevent the system from sleeping during specified time in hou
   set hours $argv[1]
   set seconds (math $hours \* 3600)
 
+  echo "Keeping computer awake for $hours hours"
   caffeinate -ut $seconds
 end
