e9067a6762e302e61ccafd8d6da329a78a566cdc
Alex Guerrero <hola@alexguerrero.es>
2023-09-26 04:25:19 +0200

    Add abbreviations for git commands

--- modules/git/init.fish
+++ modules/git/init.fish
@@ -15,5 +15,15 @@ end
 #
 
 # Git
-alias g 'git'
-alias gitc 'git commit --verbose'
+abbr g 'git'
+abbr gd 'git diff'
+abbr gc 'git commit'
+abbr gcp 'git commit --patch'
+abbr gca 'git commit --amend'
+abbr gcm 'git commit --message'
+abbr gcam 'git commit --amend --message'
+abbr gco 'git checkout'
+abbr gp 'git push'
+abbr gitc 'git commit --verbose'
+abbr gl 'git log --graph --oneline --decorate --all'
+abbr glog 'git log'
