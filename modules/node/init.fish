12e00bd39ae68c3fa504d91db1e336a524270b61
Alex Guerrero <hola@alexguerrero.es>
2025-01-21 18:57:23 +0100

    Replace nvm by pnpm

--- modules/node/init.fish
+++ modules/node/init.fish
@@ -4,19 +4,11 @@
 #
 
 # Return if requirements are not found.
-if not type -q node; and not brew list nvm &>/dev/null;
+if not type -q node; and not brew list pnpm &>/dev/null;
   exit
 end
 
-set nvm_binary (echo (brew --prefix nvm)/nvm.sh)
-
-# Load NVM package manager installed into the shell session.
-if type -q brew; and test -f $nvm_binary
-
-  mkdir -p "$HOME/.nvm"
-  and set -x NVM_DIR "$HOME/.nvm"
-
-  function nvm
-    bass source $nvm_binary --no-use ';' nvm $argv
-  end
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
 end
