8ec70986efcb12dd6e33ce630a328a6e3dcc590f
Alex Guerrero <hola@alexguerrero.es>
2025-01-22 03:52:39 +0100

    Improve node setup comand

--- modules/node/init.fish
+++ modules/node/init.fish
@@ -8,7 +8,6 @@ if not type -q node; and not brew list pnpm &>/dev/null;
   exit
 end
 
-set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
